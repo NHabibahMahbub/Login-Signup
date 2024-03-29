555c635e567b12199dd8e56db1134e2eed7282ee
NHabibahMahbub <habs101300@gmail.com>
2024-03-29 23:35:41 +0600

    login and signup process

---
+++ Registration2/registration/app1/views.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 != password2:
+            return HttpResponse("Your password didn't match!")
+        else:
+            my_user = User.objects.create_user(username, email, password1)
+            my_user.save()
+            return redirect("login")
+
+    return render(request, 'signup.html')
+
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('pass')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return HttpResponse("Username or Password you have given is incorrect. Please try again!")
+    return render(request, 'login.html')
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
